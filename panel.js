5c9935d6d2a1d23f24fd45a1d866c1e718770203
MauriSolorzano <maurysolorzano.03@gmail.com>
2026-01-08 17:08:49 -0300

    Cambio de valor en constante url de panel.js

--- panel.js
+++ panel.js
@@ -1,7 +1,7 @@
 // ==========================================
 // CONFIGURACIÓN LOCAL
 // ==========================================
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://10.0.0.50";
 const plantaID = localStorage.getItem('plantaID');
 // ==========================================
 
